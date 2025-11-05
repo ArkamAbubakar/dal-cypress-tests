0164d960f0f097ada67d71d25e60bdfaaa1d4198
Arkam Abubakar <arkam.abubakar@vetdrive.co>
2025-11-05 07:20:49 +0500

    ayn-cypress-tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true,
+    timestamp: "mmddyyyy_HHMMss"
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
-  },
+    baseUrl: "https://qa-getdal-1a.getdal.sa"
+  }
 });
